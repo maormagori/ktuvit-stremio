e4afd8d830dbfb1920b2e30f139787a7ff048313
maormagori <maormagori@gmail.com>
2020-11-06 02:09:13 +0200

    Now possible to enter a title and get Ktuvit ID back

--- bin/ktuvitHelper.js
+++ bin/ktuvitHelper.js
@@ -2,7 +2,7 @@ const superagent = require("superagent");
 
 const KTUVIT = {
     BASE_URL: "https://www.ktuvit.me/",
-    SEARCH_URL: BASE_URL+"Services/ContentProvider.svc/SearchPage_search",
+    SEARCH_URL: "https://www.ktuvit.me/Services/ContentProvider.svc/SearchPage_search",
 }
 function addHeaders(link, data){
     const headers = {
@@ -12,13 +12,56 @@ function addHeaders(link, data){
     return new Promise((resolve, reject)=>{
         superagent.post(link)
         .withCredentials()
+        //.timeout({deadline: 6000})
         .set(headers)
-        .send(data)
-        .then((res) => {resolve(res)})
+        .send({"request" :data})
+        .then((res) => {console.log("PING!"); resolve(res)})
         .catch((err) => {reject(err)})
     })
     
 }
 
+async function getKtuvitID(item){
 
-addHeaders('https://www.ktuvit.me/Services/ContentProvider.svc/SearchPage_search',{"request":{"FilmName":"breaking bad","Actors":[],"Studios":null,"Directors":[],"Genres":[],"Countries":[],"Languages":[],"Year":"2008","Rating":[],"Page":1,"SearchType":"1","WithSubsOnly":false}}).then(res=>console.log(res))
+    const query = {"FilmName": item.name,
+                 "Actors": [],
+                 "Studios": null,
+                 "Directors": [],
+                 "Genres": [],
+                 "Countries": [],
+                 "Languages": [],
+                 "Year": `${item.year}`.split("-")[0],
+                 "Rating": [],
+                 "Page": 1,
+                 "SearchType": item.type === "movie" ? "0":"1",
+                 "WithSubsOnly": false
+    };
+
+    console.log(query)
+
+    if (item.type === "movie")
+        query.SearchType = "0";
+    else
+        query.SearchType = "1";
+
+    try{
+        let res = await addHeaders(KTUVIT.SEARCH_URL,query);
+        return findIDInResults(res.body, item.imdbId);
+    } catch (err) {
+        console.log("addHeaders: Bad Request:");
+        console.log(err);
+    }
+    
+
+}
+
+function findIDInResults(body, imdbId){
+    //console.log(body);
+    console.log({ImdbId: imdbId,body: body});
+    const films = [...JSON.parse(body.d).Films]
+
+    return films.find(title => title.ImdbID == imdbId).ID;
+}
+
+
+//getKtuvitID({year: 2008, type: 'series',name: 'Breaking Bad', imdbId: 'tt0903747'}).then(id=>console.log(id));
