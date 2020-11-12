064926c5b941ef2332ba994df74573da6c043be4
maormagori <maormagori@gmail.com>
2020-11-13 01:47:29 +0200

    Working on the downloading method. Need to extract the srt from the buffer.

--- bin/ktuvitHelper.js
+++ bin/ktuvitHelper.js
@@ -1,4 +1,5 @@
-const superagent = require("superagent"),
+const superagent = require("superagent")
+    require('superagent-charset')(superagent),
     jsdom = require('jsdom');
 
 class KtuvitManager {
@@ -8,13 +9,16 @@ class KtuvitManager {
             BASE_URL: "https://www.ktuvit.me/",
             SEARCH_URL: "https://www.ktuvit.me/Services/ContentProvider.svc/SearchPage_search",
             MOVIE_INFO_URL: "https://www.ktuvit.me/MovieInfo.aspx?ID=",
-            EPISODE_INFO_URL: "https://www.ktuvit.me/Services/GetModuleAjax.ashx?"
+            EPISODE_INFO_URL: "https://www.ktuvit.me/Services/GetModuleAjax.ashx?",
+            REQUEST_DOWNLOAD_IDENTIFIER_URL: "https://www.ktuvit.me/Services/ContentProvider.svc/RequestSubtitleDownload",
+            DOWNLOAD_SUB_URL: "https://www.ktuvit.me/Services/DownloadFile.ashx?DownloadIdentifier="
         };
         this.loginCookie = loginCookie;
         this.headers = {
             "accept": "application/json, text/javascript, */*; q=0.01",
             "cookie": `Login=${this.loginCookie}`
         }
+        
     }
     
 
@@ -42,6 +46,10 @@ class KtuvitManager {
         })
     }
 
+    /**
+     * 
+     * @param {Object} item      The title to search. includes: name, year, type, imdbId. All strings.
+     */
     async getKtuvitID(item){
 
         const query = {"FilmName": item.name,
@@ -91,7 +99,7 @@ class KtuvitManager {
 
         var res = await this.getWithLoginInfo(this.KTUVIT.EPISODE_INFO_URL+query_string);
         var subtitlesIDs = this.extractSubsFromHtml(res.text);
-        console.log(subtitlesIDs);
+        //console.log(subtitlesIDs);
         return subtitlesIDs;
 
     }
@@ -111,7 +119,7 @@ class KtuvitManager {
         var subtitlesListElement = dummyDom.document.getElementById('subtitlesList');
         subtitlesListElement = [...subtitlesListElement.rows];
         subtitlesListElement.shift();
-        console.log(subtitlesListElement.length);
+        //console.log(subtitlesListElement.length);
         var subtitlesIDs = subtitlesListElement.map((sub) => {
             //Getting sub's file name from html.
             //I don't care it's this way, it works.
@@ -127,11 +135,44 @@ class KtuvitManager {
         });
 
         return subtitlesIDs;
-
     }
+
+    async downloadSubtitle(KtuvitId, subId, cb, shouldDecode=false){
+        const downloadIdentifierRequest = {
+            "FilmID": KtuvitId,
+            "SubtitleID": subId,
+            "FontSize": 0,
+            "FontColor": "",
+            "PredefinedLayout": -1
+        };
+
+        let downloadIdentifier = await this.postWithLoginInfo(this.KTUVIT.REQUEST_DOWNLOAD_IDENTIFIER_URL,downloadIdentifierRequest);
+        downloadIdentifier = JSON.parse(downloadIdentifier.body.d).DownloadIdentifier
+        
+        await superagent.get(this.KTUVIT.DOWNLOAD_SUB_URL + downloadIdentifier)
+            .charset('ISO-8859-8')
+            .withCredentials()
+            .set(this.headers)
+            .buffer()
+            .then(res => {cb(res)})
+            .catch(err => err)
+    }
+
+    //Not using this parser. Might be useful to implement it.
+    // parseBuffer(res, resolve){
+    //     let buffer = [];
+    //     res.on('data', (chunk)=>{
+    //         buffer.push(iconv.decode(chunk, 'ISO 8859-8'));
+    //     });
+    //     res.on('end', ()=>
+    //         resolve(null, Buffer.concat(buffer)));
+    // }
+
 }
 
 const manager = new KtuvitManager("u=7CA271EC2204B13FAE3F3CFE9D24F3AC&g=3B82622A00E8D3D24F982498638320F48803A3A8CED4220DEDCFBE2A06219528853A8A8AFC7589346C15A2979E58EC07")
 //manager.getKtuvitID({year: 2008, type: 'series',name: 'Breaking Bad', imdbId: 'tt0903747'}).then(id=>console.log(id));
 //manager.getSubsIDsListMovie("3E488DFBF1B06910D8EAD33F60680FF9");
-manager.getSubsIDsListEpisode("C6DCA1F5779E310E8FC2DD53AC4C40FC",4,8);
+//manager.getSubsIDsListEpisode("C6DCA1F5779E310E8FC2DD53AC4C40FC",4,8);
+
+manager.downloadSubtitle('C6DCA1F5779E310E8FC2DD53AC4C40FC','41E986A04698829FFCE5CFD430C7F4F3',(buffer) => {console.log(buffer)},true);
